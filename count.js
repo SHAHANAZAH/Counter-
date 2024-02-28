bbba2b3faae2a45f8a4a765bff7136f2bb465e55
SHAHANAZ A H <83229840+SHAHANAZAH@users.noreply.github.com>
2024-02-28 22:22:53 +0530

    Add files via upload

---
+++ count.js
@@ -0,0 +1,18 @@
+let count=0;
+document.getElementById("desc").onclick=function(){
+    count-=1;
+document.getElementById("countlabel").innerHTML=count;
+}
+
+
+document.getElementById("reset").onclick=function(){
+    count=0;
+document.getElementById("countlabel").innerHTML=count;
+}
+
+
+document.getElementById("inc").onclick=function(){
+    count+=1;
+document.getElementById("countlabel").innerHTML=count;
+}
+
